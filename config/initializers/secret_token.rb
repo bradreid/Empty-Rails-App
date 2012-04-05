bccf8bfaddd03f5751f3bebddb01255469c46ed0
Brad Reid <brad.reid@canadadrugs.com>
2012-04-05 09:30:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookAuth::Application.config.secret_token = '2ac803098cfd69b7a73e06ef33ee1009ea42a40fc741a62382e443702966ab15a9b71ec77236c82072312936c5b56d2d4da36b4918f99866255b3434b1a036f4'
